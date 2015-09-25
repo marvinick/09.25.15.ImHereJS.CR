d874a1b8e74929f6f5b7e0fd7ee5f0b359bc1c7e
marvinick <marviluw@gmail.com>
2015-09-25 09:17:32 -0700

    this commit will have a welcome page and welcome route

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 var imHere = angular.module('imHere', ['ui.router']);
+
+imHere.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('welcome', {
+    url:'',
+    templateUrl: "partials/welcome.html"
+  });
+});
