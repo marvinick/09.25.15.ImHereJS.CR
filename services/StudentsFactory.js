d9f240bb13c7e8d73708c07a097dcb4ed76e28f9
marvinick <marviluw@gmail.com>
2015-09-25 13:03:56 -0700

    this commit will add styling

--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,6 +1,13 @@
 imHere.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
-  factory.students = [];
+  factory.students = [
+    { name: "Pedro", signIn: false },
+    { name: "Mamacita", signIn: false },
+    { name: "Pepe", signIn: false },
+    { name: "Hefe", signIn: false },
+    { name: "Chica", signIn: false }
+  ];
+
   factory.addStudent = function() {
     factory.students.push({ name: factory.studentName, signIn: false });
     factory.studentName = null;
